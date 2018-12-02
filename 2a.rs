e0d2ee8faa3bbbcd4cb9251ba6428a4fd2ece621
Juha Paananen <juha.paananen@gmail.com>
2018-12-02 20:16:15 +0200

    cleanup

--- 2a.rs
+++ 2a.rs
@@ -2,7 +2,6 @@ extern crate itertools;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::fs::File;
-use std::collections::HashSet;
 use itertools::Itertools;
 
 fn lines_from_file<P>(filename: P) -> Result<io::Lines<io::BufReader<File>>, io::Error>
@@ -16,7 +15,7 @@ fn has_count(count: usize, str: &String) -> bool {
     let mut vec = str.chars().collect::<Vec<char>>();
     vec.sort();
     let grouped = vec.into_iter().group_by(|&c| c);
-    let mut counts = grouped.into_iter().map(|(k, group)| group.count());
+    let mut counts = grouped.into_iter().map(|(_, group)| group.count());
     counts.any(|c| c == (count as usize))
 }
 
--- 2b.rs
+++ 2b.rs
@@ -2,8 +2,6 @@
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::fs::File;
-use std::collections::HashSet;
-use itertools::Itertools;
 
 fn lines_from_file<P>(filename: P) -> Result<io::Lines<io::BufReader<File>>, io::Error>
   where P: AsRef<Path>,
@@ -17,7 +15,7 @@ fn count_diff(x: &String, y: &String) -> usize {
 }
 
 fn common_letters(x: &String, y: &String) -> String {
-    izip!(x.chars(), y.chars()).filter(|&(x, y)| x == y).map(|(x, y)| x).collect()
+    izip!(x.chars(), y.chars()).filter(|&(x, y)| x == y).map(|(x, _)| x).collect()
 }
 
 fn main() {
@@ -47,7 +45,6 @@ impl<A : Clone, I: Clone + Iterator<Item = A>> Iterator for PairIter<A, I> {
     type Item = (A, A);
 
     fn next(&mut self) -> Option<(A, A)> {
-        let mut need_new = false;
         let mut b: Option<A> = None;
         let mut new_a = None;
         match self.i2.as_mut() {
@@ -84,7 +81,5 @@ impl<A : Clone, I: Clone + Iterator<Item = A>> Iterator for PairIter<A, I> {
                 }
             }
         }
-        None
     }
 }
-
