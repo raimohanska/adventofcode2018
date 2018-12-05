62468e9afe4159e42e0037c9d0b379d5053abda4
Juha Paananen <juha.paananen@gmail.com>
2018-12-05 21:57:26 +0200

    Solutions for day 5 in Haskell

---
+++ 5b.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TupleSections, LambdaCase, FlexibleContexts #-}
+module Main where
+import Text.Parsec
+import Data.Char(digitToInt, toLower, toUpper, isSpace)
+import Data.List(tails, foldl', sort, group, intersect)
+import Data.Maybe(maybeToList, listToMaybe, fromMaybe)
+import Data.Either(rights)
+import GHC.Exts(sortWith, groupWith)
+import qualified Data.Set as S
+
+main = readFile "5.txt" >>= (return . solve . removeWhitespace) >>= print
+
+solve input = foldl1 min $ map reactedLength $ map (removeFrom input) ['A'..'Z'] 
+
+removeFrom xs c = filter ((/= c) . toUpper) xs
+
+reactedLength input = length $ reactChars input
+
+removeWhitespace = filter (not . isSpace)
+
+reactChars input = react isOpposite "" input
+
+check output = check' ' ' output
+  where check' c (x:xs) | c == x = False
+                        | otherwise = check' c xs
+        check' c []     = True
+
+-- Not the right answer!!
+--
+react :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+react f [] [] = []
+react f xs [] = xs
+react f [] (x:xs) = react f [x] xs
+react f (x:xs) (y:ys) | f x y = react f xs ys
+                    | otherwise = react f (y : x : xs) ys
+
+isOpposite a b = (a /= b) && (toLower a == toLower b)
