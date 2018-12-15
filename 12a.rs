721e78a5b835131d799054166afbbb96998c9e47
Juha Paananen <juha.paananen@gmail.com>
2018-12-15 20:16:26 +0200

    day 12 first part in Rust

--- 12a.rs
+++ 12a.rs
@@ -7,8 +7,8 @@ use std::fmt;
 #[macro_use]
 extern crate nom;
 
-const rule_width: usize = 5;
-const rule_margin: usize = (rule_width - 1) / 2;
+const RULE_WIDTH: usize = 5;
+const RULE_MARGIN: usize = (RULE_WIDTH - 1) / 2;
 
 type Rule = (Vec<bool>, bool);
 
@@ -19,33 +19,49 @@ struct State {
 }
 
 impl State {
-    fn ensure_capacity(&mut self) {
-        if !self.has_capacity() {
-            let margin = 10;
-            self.start_index = self.start_index - margin;
-            let mut new_plants = vec!(false; margin as usize);
-            new_plants.append(&mut self.plants);
-            self.plants = new_plants;
-        }
+    fn add_margin(&mut self) {
+        let margin = 4;
+        self.start_index = self.start_index - margin;
+
+        let mut new_plants = vec!(false; margin as usize);
+        new_plants.append(&mut self.plants);
+        new_plants.append(&mut vec!(false; margin as usize));
+        self.plants = new_plants;
     }
-    fn has_capacity(&self) -> bool {
-        for i in 0..rule_margin {
-            if self.plants[i] { 
-                return false;
-            }
-        }
-        true
+    fn evolve(&mut self, rules: &Vec<Rule>) -> State {
+        self.add_margin();
+        let plants: Vec<bool> = self.plants.windows(RULE_WIDTH).map(|slice| {
+           let rule: &Rule = rules.iter().find(|rule| match_rule(rule, slice)).unwrap(); 
+           rule.1
+        }).collect();
+        State { start_index: self.start_index + 2, plants }
     }
-    fn evolve(self, rules: &Vec<Rule>) -> State {
+}
+
+fn match_rule(r: &Rule, slice: &[bool]) -> bool {
+    let pattern: &[bool] = &r.0;
+    slice == pattern
+}
+
+fn print_state(s: &State) {
+    print!("{:?} ", s.start_index);
+    for b in &s.plants {
+       print!("{}", if *b { "#" } else { "." }) 
     }
+    println!();
 }
 
 fn main() {
    let lines: Vec<String> = lines_from_file("12.txt") .unwrap() .map(|x| x.unwrap()).collect();
    let mut state = initial_state(&lines[0]).unwrap().1;
-   let instructions = lines.iter().skip(2).map(|line| rule(line).unwrap().1).collect::<Vec<_>>();
-   state.ensure_capacity();
-   println!("{:?}", state);
+   let rules = lines.iter().skip(2).map(|line| rule(line).unwrap().1).collect::<Vec<_>>();
+   print_state(&state);
+   for i in 1..=20 {
+     state = state.evolve(&rules);
+     print_state(&state);
+   }
+   let sum: i32 = state.plants.iter().enumerate().filter(|(i, s)| **s).map(|(i, _)| (i as i32) + state.start_index).sum();
+   println!("{:?}", sum);
 }
 
 fn lines_from_file<P>(filename: P) -> Result<io::Lines<io::BufReader<File>>, io::Error>
